47bff054c858f0ccb188870e8b80d6cc30a15f98
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2024-12-24 08:38:20 -0700

    Minor cleanup of Serial initialization

--- StratoCore_RATS.ino
+++ StratoCore_RATS.ino
@@ -54,27 +54,24 @@ void setup()
   digitalWrite(HEARTBEAT_LED_PIN, LOW);
 
   Serial.begin(115200);
-  Serial.print("StratoCore_RATS build ");
-  Serial.print(__DATE__);
-  Serial.print(" ");
-  Serial.println(__TIME__);
-
   ZEPHYR_SERIAL.begin(115200);
   MCB_SERIAL.begin(115200);
 
-#if (ZEPHYR_COMMS_ON_DEBUG_PORT)
-    log_error("Configured for sending Zephyr msgs to debug port\n**** WILL NOT WORK FOR FLIGHT OPERATIONS!");
-    //Increase serial buffer sizes for Teensy 4.1
-    MCB_SERIAL.addMemoryForRead(&Zephyr_serial_RX_buffer, sizeof(Zephyr_serial_RX_buffer));
-    MCB_SERIAL.addMemoryForWrite(&Zephyr_serial_TX_buffer, sizeof(Zephyr_serial_TX_buffer));
-#else
-    //Increase serial buffer sizes for Teensy 4.1
+  Serial.println(String("StratoCore_RATS build ") + __DATE__ + " " + __TIME__);
+
+#if not ZEPHYR_COMMS_ON_DEBUG_PORT
+    // Zephyr serial is on digital I/O pins
     ZEPHYR_SERIAL.addMemoryForRead(&Zephyr_serial_RX_buffer, sizeof(Zephyr_serial_RX_buffer));
     ZEPHYR_SERIAL.addMemoryForWrite(&Zephyr_serial_TX_buffer, sizeof(Zephyr_serial_TX_buffer));
+    log_nominal("Configured for standard Zephyr port");
+#else
+    // Zephyr serial is on USB so buffer doesn't need to be increased
+    log_error("Configured for sending Zephyr msgs to debug port\n**** WILL NOT WORK FOR FLIGHT OPERATIONS!");
+#endif
+
+    // MCB serial will always be on digital I/O pins
     MCB_SERIAL.addMemoryForRead(&Zephyr_serial_RX_buffer, sizeof(Zephyr_serial_RX_buffer));
     MCB_SERIAL.addMemoryForWrite(&Zephyr_serial_TX_buffer, sizeof(Zephyr_serial_TX_buffer));
-    log_nominal("Configured for standard Zephyr port");
-#endif
 
   // Timer interrupt setup for main loop timing
   Timer1.initialize(100000); // 0.1 s
